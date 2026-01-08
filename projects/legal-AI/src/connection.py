77d4291e5288313802ed8b7aaecc3780de3028c9
Clyding <cbaidoo99@gmail.com>
2026-01-08 13:03:08 -0500

    db-setup

---
+++ projects/legal-AI/src/connection.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+def mongodb_connection(db_name, collection_name):
+    client = MongoClient('mongodb://admin:admin@mongodb:27017/')
+
+    db = client[db_name]
+    collection = db[collection_name]
+
+    return db, collection
+
+def postgre_connection():
+    conn = psycopg2.connect(
+    dbname="airflow",
+    user="airflow",
+    password="airflow",
+    host="postgres", 
+    port="5432"     
+)
+
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    
+    return conn, cur
